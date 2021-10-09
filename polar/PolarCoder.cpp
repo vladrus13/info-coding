68143e1601c12bbe35683fb20b0d7cb601adeece
Vladislav Kuznetsov <Vladislav.Kuznetsov@jetbrains.com>
2021-10-09 18:50:31 +0300

    Encode polar codes added

--- polar/PolarCoder.cpp
+++ polar/PolarCoder.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "PolarCoder.h"
+
+std::vector<int> PolarCode::encode(const std::vector<int> &info) {
+    std::vector<int> bits_padded(block_size, 0);
+    std::vector<int> returned(block_size);
+    for (int i = 0; i < len; i++) {
+        bits_padded[channel[i]] = info[i];
+    }
+    for (int i = len; i < len + crc; ++i) {
+        int crc_bit = 0;
+        for (int j = 0; j < len; ++j) {
+            crc_bit = (crc_bit + matrix[i - len][j] * info[j]) % 2;
+        }
+        bits_padded[channel[i]] = crc_bit;
+    }
+    for (int nest = 0; nest < n; ++nest) {
+        int inc = 1 << nest;
+        for (int j = 0; j < inc; j += 1) {
+            for (int i = 0; i < len; i += 2 * inc) {
+                bits_padded[i + j] = (bits_padded[i + j] + bits_padded[i + j + inc]) % 2;
+            }
+        }
+    }
+    for (int i = 0; i < len; ++i) {
+        returned[i] = bits_padded[reverse_order[i]];
+    }
+    return returned;
+}
+
+std::vector<int> PolarCode::decode(const std::vector<double>& p0, const std::vector<double>& p1, int L) {
+
+}
--- polar/PolarCoder.h
+++ polar/PolarCoder.h
@@ -5,4 +5,74 @@
 #ifndef INFO_CODING_POLARCODER_H
 #define INFO_CODING_POLARCODER_H
 
+#include <vector>
+#include <algorithm>
+
+class PolarCode {
+private:
+    int n;
+    int len;
+    int block_size;
+    int crc;
+    double epsilon;
+    std::vector<int> frozen;
+    std::vector<int> reverse_order;
+    std::vector<int> channel;
+    std::vector<std::vector<int>> matrix;
+
+public:
+    PolarCode(int step, double epsilon, int crc)
+            : n(step), len(1 >> (step - 1)), epsilon(epsilon), crc(crc) {
+        block_size = (1 << step);
+        frozen.resize(block_size, 0);
+        reverse_order.resize(block_size);
+
+        for (int i = 0; i < block_size; i++) {
+            int reversed = i;
+            reverse_order[i] = ((reversed & 1) << (n - 1));
+            for (int j = n - 1; j; j--) {
+                reversed >>= 1;
+                reverse_order[i] -= (reversed & 1) << (j - 1);
+            }
+        }
+
+        std::vector<double> channel_mistake(block_size, epsilon);
+        for (int nest = 0; nest < n; nest++) {
+            int inc = 1 << nest;
+            for (int j = 0; j < inc; j++) {
+                for (int i = 0; i < block_size; i += 2 * inc) {
+                    double first_channel = channel_mistake[i + j];
+                    double second_channel = channel_mistake[i + j + inc];
+                    channel_mistake[i + j] = first_channel + second_channel - first_channel * second_channel;
+                    channel_mistake[i + j + inc] = first_channel * second_channel;
+                }
+            }
+        }
+
+        channel.resize(block_size);
+        for (int i = 0; i < block_size; i++) {
+            channel[i] = i;
+        }
+        std::sort(std::begin(channel),
+                  std::end(channel),
+                  [&](int i1, int i2) {
+                      return channel_mistake[reverse_order.at(i1)] < channel_mistake[reverse_order.at(i2)];
+                  });
+        int record_frozen = len + crc;
+        for (int i = 0; i < record_frozen; i++) {
+            frozen[channel[i]] = 0;
+        }
+        matrix.resize(crc);
+        for (int bit = 0; bit < crc; bit++) {
+            matrix[bit].resize(len);
+            for (int info = 0; info < len; info++) {
+                matrix[bit][info] = std::rand() % 2;
+            }
+        }
+    }
+
+    std::vector<int> encode(const std::vector<int> &info);
+    std::vector<int> decode(const std::vector<double>& p0, const std::vector<double>& p1, int L);
+};
+
 #endif //INFO_CODING_POLARCODER_H
