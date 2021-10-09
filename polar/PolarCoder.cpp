164b247e1db64abb21254ff09a1db501fe1b8394
Vladislav Kuznetsov <Vladislav.Kuznetsov@jetbrains.com>
2021-10-09 16:05:17 +0300

    Initial commit

---
+++ polar/PolarCoder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Vladislav.Kuznetsov on 09.10.2021.
+//
+
+#include "PolarCoder.h"
---
+++ polar/PolarCoder.h
@@ -0,0 +1,8 @@
+//
+// Created by Vladislav.Kuznetsov on 09.10.2021.
+//
+
+#ifndef INFO_CODING_POLARCODER_H
+#define INFO_CODING_POLARCODER_H
+
+#endif //INFO_CODING_POLARCODER_H
